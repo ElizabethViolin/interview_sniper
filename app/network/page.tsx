6db5547662eaea5169d3ed0dace218a97f44f557
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-04-16 00:45:23 +0800

    frontend: web and mobile responsive design

--- app/network/page.tsx
+++ app/network/page.tsx
@@ -7,9 +7,9 @@ import Post from './components/post';
 
 export default function NetworkPage() {
   return (
-    <main className='h-full w-full bg-gradient-to-r from-blue-700 to-transparent to-blue-700'>
+    <main className='h-full w-full bg-gradient-to-r from-blue-700 to-transparent to-blue-700 px-16 md:px-0'>
       <div className='flex flex-col justify-center items-center py-10'>
-        <div className='flex flex-col justify-between h-fit w-1/2 bg-[rgba(229,229,229,0.2)] rounded-xl p-10 space-y-5'>
+        <div className='flex flex-col justify-between h-fit w-full max-w-3xl bg-[rgba(229,229,229,0.2)] rounded-xl p-10 space-y-5'>
           <div className='space-y-5 text-white'>
             <TextareaAutosize rows={1} maxRows={2} className='w-full bg-transparent border-none resize-none outline-none border-none focus:outline-none focus:ring-0 text-lg' placeholder='Company name' /> 
             <TextareaAutosize rows={4} maxRows={20} className='w-full bg-transparent border-none resize-none outline-none border-none focus:outline-none focus:ring-0' placeholder='Share your interview experience...' /> 
