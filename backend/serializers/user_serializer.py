f872ee6f8542e719ad404cbb4d88f9afcabecc3a
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-04-24 16:55:25 +0800

    backend: Add users/me endpoint for current user information retrieval

--- backend/serializers/user_serializer.py
+++ backend/serializers/user_serializer.py
@@ -4,4 +4,4 @@ from backend.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'headline', 'phone_number']
+        fields = ['id', 'username', 'email', 'headline', 'phone_number', 'first_name', 'last_name']
--- backend/viewsets/user_viewset.py
+++ backend/viewsets/user_viewset.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from backend.models import User
 from backend.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    @action(detail=False, methods=['get'], url_path='me')
+    def current_user(self, request):
+        """
+        Returns the details of the current user.
+        """
+        if request.user.is_authenticated:
+            serializer = self.get_serializer(request.user)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'User is not authenticated'}, status=401)
