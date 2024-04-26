88fd4b9273eb9ef31bb0434921adebc7aac425fe
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-04-26 23:50:29 +0800

    backend: add filter queryset to Post Viewset to show only posts bookmarked by user

--- backend/viewsets/post_viewset.py
+++ backend/viewsets/post_viewset.py
@@ -5,11 +5,21 @@ from backend.models import Post, Bookmark, Reaction, Question
 from backend.serializers import PostSerializer
 from django.db.models import Exists, OuterRef, Subquery
 
+
 class PostViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for handling CRUD operations on posts.
+    """
+
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    SHOW_USER_POSTS = "mine"
+    SHOW_COLLECTION_POSTS = "bookmarked"
 
     def get_queryset(self):
+        """
+        Get the queryset based on the request parameters.
+        """
         user = self.request.user 
 
         # Subquery for fetching the latest reaction type by the user
@@ -24,8 +34,23 @@ class PostViewSet(viewsets.ModelViewSet):
             ),
             user_reaction=Subquery(reaction_subquery)
         )
-
+        
         # Filter to show only user's posts if 'mine' query parameter is present
-        if 'mine' in self.request.query_params:
-            queryset = queryset.filter(created_by=user)
+        if self.request.query_params.get(self.SHOW_USER_POSTS):
+            queryset = self.filter_user_posts(queryset, user)
+        # Filter to show only user's posts that user has bookmarked
+        elif self.request.query_params.get(self.SHOW_COLLECTION_POSTS):
+            queryset = self.filter_bookmarked_posts(queryset)
         return queryset
+
+    def filter_user_posts(self, queryset, user):
+        """
+        Filter queryset to show only posts created by the user.
+        """
+        return queryset.filter(created_by=user)
+
+    def filter_bookmarked_posts(self, queryset):
+        """
+        Filter queryset to show only posts bookmarked by the user.
+        """
+        return queryset.filter(is_bookmarked=True)
